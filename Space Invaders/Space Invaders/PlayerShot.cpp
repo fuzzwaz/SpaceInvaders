858986e420fa580532779a4e6ddb656fe629c077
fuzzwaz <Fuzzwaz@Ramys-MacBook-Air.local>
2015-07-28 00:37:12 -0400

    Converted Game Manager to Singleton

---
+++ Space Invaders/Space Invaders/PlayerShot.cpp
@@ -0,0 +1,9 @@
+//
+//  PlayerShot.cpp
+//  Space Invaders
+//
+//  Created by Ramy Fawaz on 7/24/15.
+//  Copyright (c) 2015 Ramy Fawaz. All rights reserved.
+//
+
+#include "PlayerShot.h"
---
+++ Space Invaders/Space Invaders/PlayerShot.h
@@ -0,0 +1,14 @@
+//
+//  PlayerShot.h
+//  Space Invaders
+//
+//  Created by Ramy Fawaz on 7/24/15.
+//  Copyright (c) 2015 Ramy Fawaz. All rights reserved.
+//
+
+#ifndef __Space_Invaders__PlayerShot__
+#define __Space_Invaders__PlayerShot__
+
+#include <stdio.h>
+
+#endif /* defined(__Space_Invaders__PlayerShot__) */
